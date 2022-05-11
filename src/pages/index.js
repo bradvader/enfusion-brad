bd0187fd7504d59237420105c8f5b181a09ced3f
bradvader <vader.brad@gmail.com>
2022-05-11 10:07:31 -0400

    Basic environment setup

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Enfusion - Brad Vader</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="min-h-screen flex flex-col justify-center">
+        <h1 className="mx-auto text-4xl font-bold">
+          Enfusion Home Page - Brad Vader
+        </h1>
+      </main>
+
+    </div>
+  )
+}
