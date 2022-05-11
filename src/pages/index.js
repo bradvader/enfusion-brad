b8292071155311fe5bac2624e484aba9c872e9e7
bradvader <vader.brad@gmail.com>
2022-05-11 10:58:47 -0400

    And another one

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Enfusion - Brad Vader</title>
+        <title>Enfusion | Brad Vader</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="min-h-screen flex flex-col justify-center">
         <h1 className="mx-auto text-4xl font-bold">
-          Enfusion Home Page - Brad Vader Testtttt
+          Enfusion Home Page - Brad Vader Heading
         </h1>
       </main>
 
